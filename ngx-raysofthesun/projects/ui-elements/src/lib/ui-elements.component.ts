13f9e0bb016c6df3d1837b26cb9c79d438bcf10e
Carl <rdelsol35@gmail.com>
2021-08-31 22:02:40 +0800

    setup workspaces

---
+++ ngx-raysofthesun/projects/ui-elements/src/lib/ui-elements.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'raysofthesun-ui-elements',
+  template: `
+    <p>
+      ui-elements works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UiElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ngx-raysofthesun/projects/ui-elements/src/lib/ui-elements.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { UiElementsComponent } from './ui-elements.component';
+
+
+
+@NgModule({
+  declarations: [UiElementsComponent],
+  imports: [
+  ],
+  exports: [UiElementsComponent]
+})
+export class UiElementsModule { }
